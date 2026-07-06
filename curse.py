b0d1b5ad968c7690105adb6d603c1429d3cf4427
Maddison Hoveida <mohaddesehhoveida@yahoo.com>
2026-07-06 10:13:21 -0400

    Refactor comments for clarity and completeness

--- curse.py
+++ curse.py
@@ -9,15 +9,15 @@ ax.set(xlabel='dimensions (m)', ylabel='log(dmax/dmin)', title='dmax/dmin vs. di
 line_styles = {0: 'ro-', 1: 'b^-', 2: 'gs-', 3: 'cv-'}
 
 # Plot dmax/dmin ratio
-# TODO: fill in valid test numbers
+# fill in valid test numbers
 sample_size_list = [100, 500, 1000, 5000]
 
 for idx, num_samples in enumerate(sample_size_list):
-    # TODO: Fill in a valid feature range
+    # Fill in a valid feature range
     feature_range = range (1, 101)
     ratios = []
     for num_features in feature_range:
-        # TODO: Generate synthetic data using make_classification
+        # Generate synthetic data using make_classification
         X, _ = make_classification(
             n_samples=num_samples,
             n_features=num_features,
@@ -28,15 +28,15 @@ for idx, num_samples in enumerate(sample_size_list):
             n_classes=1,
             random_state=42
         )
-        # TODO: Choose random query point from X
+        # Choose random query point from X
         #query_point = X[np.random.randint(0, num_samples)]
         query_index = np.random.randint(0, len(X))
         query_point = X[query_index]
         
-        # TODO: remove query pt from X so it isn't used in distance calculations
+        # remove query pt from X so it isn't used in distance calculations
         X_except_query = np.delete(X, query_index, axis = 0)
 
-        # TODO: Calculate distances
+        # Calculate distances
         distances = np.linalg.norm(X_except_query - query_point, axis=1)
         ratio = np.max(distances) / np.min(distances)
         ratios.append(ratio)
